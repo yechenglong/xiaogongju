2ae6d0717327c8f887311520b17b09423100f10a
A1401021150\Administrator <leaf.ye@cygia.com>
2018-09-03 17:54:34 +0800

    爬取携程机票

--- 得到每天的机票价格.py
+++ 得到每天的机票价格.py
@@ -5,8 +5,7 @@ startdate='20181007'
 enddate='20181009'
 startDate=datetime.datetime.strptime(startdate,'%Y%m%d')
 endDate=datetime.datetime.strptime(enddate,'%Y%m%d')
-print(startDate)
-print(endDate)
+finalldata = {}
 dcity = {"ZUH": "珠海", "CAN": "广州", "SZX": "深圳"}
 acity = {"SHA": "上海", "HGH": "杭州", "NGB": "宁波"}
 url = "http://flights.ctrip.com/itinerary/api/12808/lowestPrice"
@@ -17,7 +16,16 @@ for fromcode, fromcity in sorted(dcity.items(), key=itemgetter(0)):
         if fromcode != tocode:
             text = requests.post(url,json=json1)
             data = json.loads(text.text)
-            cost = data['data']['oneWayPrice'][0]['20180901']
+            data1 = data['data']['oneWayPrice'][0]
+            for date, price in data1.items():
+                newdate = datetime.datetime.strptime(date, '%Y%m%d')
+                if newdate <= endDate and newdate >= startDate:
+                    city = fromcity + "-->" + tocity + " : " + str(newdate)
+                    finalldata[city] = price
+
+finallprice = sorted(finalldata.items(), key=itemgetter(1))
+print(finallprice[0])
+
 
 
 
