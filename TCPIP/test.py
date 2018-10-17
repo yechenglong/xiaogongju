bb4ddead68bda9d59b86b23c540353142cca240f
A1401021150\Administrator <leaf.ye@cygia.com>
2018-10-17 18:01:23 +0800

    设计了client ui

--- TCPIP/test.py
+++ TCPIP/test.py
@@ -5,13 +5,16 @@ from PyQt5.QtWidgets import *
 from TCPIP.mainWindow import Ui_NetTools
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from TCPIP.client import Ui_Form
 
 class MyMainWindow(QMainWindow, Ui_NetTools,QPushButton):
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
-        self.count = 1
         self.createContextMenu()
+        self.child1 = ChildrenForm()
+
+
 
     def createContextMenu(self):
         '''
@@ -24,29 +27,52 @@ class MyMainWindow(QMainWindow, Ui_NetTools,QPushButton):
         self.contextMenu = QMenu(self)
         self.actionnew = self.contextMenu.addAction("New")
         self.actiondelete = self.contextMenu.addAction("Delete")
-        self.actionnew.triggered.connect(self.actionHandler)
-        self.actiondelete.triggered.connect(self.actionHandler)
+        self.actionnew.triggered.connect(self.newaction)
+        self.actiondelete.triggered.connect(self.deleteaction)
+
 
     def showContextMenu(self, pos):
         '''''
         右键点击时调用的函数
         '''
-        self.count+=1
         # 菜单显示前，将它移动到鼠标点击的位置
+
+        if self.treeWidget.selectedItems() != []:
+            self.actionnew.setVisible(False)
+        else:
+            self.actionnew.setVisible(True)
         self.contextMenu.exec_(QCursor.pos()) #在鼠标位置显示
         #self.contextMenu.show()
-        print(self.count)
 
-    def actionHandler(self):
+    def newaction(self):
         '''''
-        菜单中的具体action调用的函数
+        菜单中的具体actionnew调用的函数
         '''
-        if self.count % 3 == 1:
-            self.setText(u"first")
-        elif self.count % 3 == 2:
-            self.setText(u"second")
-        elif self.count % 3 == 0:
-            self.setText(u"third")
+        self.child = QTreeWidgetItem(self.treeWidget.currentItem())
+        self.child.setText(0,'client')
+        self.treeWidget.expandItem(self.treeWidget.currentItem())
+        self.childShow()
+
+    def deleteaction(self):
+        '''''
+        菜单中的具体actiondelete调用的函数
+        '''
+        for item in self.treeWidget.selectedItems():
+            (item.parent() or self.child).removeChild(item)
+        self.verticalLayout.removeWidget(self.child1)
+
+    def childShow(self):
+        self.verticalLayout.addWidget(self.child1)
+        self.child1.show()
+
+
+class ChildrenForm(QTabWidget,Ui_Form):
+    def __init__(self):
+        super(ChildrenForm,self).__init__()
+        self.setupUi(self)
+
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
